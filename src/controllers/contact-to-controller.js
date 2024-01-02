b0b75b866acdbfe2dc236510518bc59e20703a03
mukesh kumar <mkmehta2041@gmail.com>
2024-01-02 13:12:19 +0530

    send thank you mail and demo mail

--- src/controllers/contact-to-controller.js
+++ src/controllers/contact-to-controller.js
@@ -1,17 +1,20 @@
 const CatchAsyncError = require("../middleware/catchAsyncError");
 const Contact = require("../models/contact.model");
 const ErrorHandler = require("../utils/ErrorHandler");
+const sendMail = require("../utils/sendMail");
+const ejs = require("ejs");
+const path = require("path");
 
 const toContact = CatchAsyncError(async (req, res, next) => {
   try {
     const { firstName, lastName, phone, email, message, type } = req.body;
 
-    if (!firstName || !lastName || !phone || !email || !message) {
+    if (!firstName || !lastName || !phone || !email || !message || !type) {
       throw new Error("Please provide all required fields");
     }
 
     // create a record for contact
-    await Contact.create({
+    const contact = await Contact.create({
       firstName,
       lastName,
       phone,
@@ -20,12 +23,34 @@ const toContact = CatchAsyncError(async (req, res, next) => {
       Type: type,
     });
 
-    // send a thank you email
+    if (!contact) {
+      throw new Error("Please provide all required fields");
+    }
 
-    return res.status(200).json({
-      success: true,
-      message: "Message sent successfully",
-    });
+    // send a thank you email
+    const data = { user: { name: firstName } };
+    const template = type === "Contact" ? "contact-mail.ejs" : "demo-mail.ejs";
+
+    console.log(template);
+
+    try {
+      await sendMail({
+        email: contact.email,
+        subject: "Contact Information",
+        template: template,
+        data,
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "Message sent successfully",
+      });
+    } catch (error) {
+      throw new ErrorHandler(
+        "Error sending Thank you email: " + error.message,
+        500
+      );
+    }
   } catch (error) {
     return next(new ErrorHandler(error.message, 400));
   }
