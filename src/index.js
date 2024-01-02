b69c594e5676891e22be36884720284e631d48f7
mukesh kumar <mkmehta2041@gmail.com>
2024-01-02 23:00:14 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./app/app");
 const dbConnect = require("./config/dbConnect");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
 // create server
 app.listen(PORT, () => {
