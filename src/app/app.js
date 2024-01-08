3ab50146ae409cfda156b8d3a5b275e5b9ec6c40
mukesh kumar <mkmehta2041@gmail.com>
2024-01-08 17:51:00 +0530

    tiny bugs

--- src/app/app.js
+++ src/app/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 // Rate limiting middleware
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200, // limit each IP to 100 requests per windowMs
+  max: 400, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 // v1 routes
